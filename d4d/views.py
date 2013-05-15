6c1743e89188039c126fc25883c13fc156bc25fb
Ubuntu <ubuntu@ip-10-34-17-18.ec2.internal>
2013-05-15 00:08:45 +0000

    still static initially, but 2 levels deep on explor

--- d4d/views.py
+++ d4d/views.py
@@ -6,16 +6,22 @@ from django.template import RequestContext
 
 # Create your views here.
 
+explorer = d4dExplorer()
+
+def get_children(concept, exclude):
+  explorer.concept = concept
+  return [{"id": str(s[0]), "name": str(s[0]), "data": [], "children": []} for s in explorer.get_similar() if str(s[0]) not in exclude]
+  
+
 def visualize(request):
   return render_to_response('index.html', {},
          context_instance=RequestContext(request))
 
 def index(request, query):
-  explorer = d4dExplorer()
   explorer.concept = query
   similar = explorer.get_similar()
   out = {"id": query,
          "name": query,
          "data": [],
-         "children": [{"id": str(s[0]), "name": str(s[0]), "data": [], "children": []} for s in similar]}
+         "children": [{"id": str(s[0]), "name": str(s[0]), "data": [], "children": get_children(str(s[0]), query)} for s in similar]}
   return HttpResponse(json.dumps(out))
