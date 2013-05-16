5946ec58bb7a3b1356e0e1abc534ef94520d02c3
Ubuntu <ubuntu@ip-10-242-117-5.ec2.internal>
2013-05-16 23:35:35 +0000

    similar assertions mostly working, getting similarity score in for line width

--- d4d/js/example1.js
+++ d4d/js/example1.js
@@ -21,9 +21,11 @@ var Log = {
     if (!this.elem) 
       this.elem = document.getElementById('log');
     this.elem.innerHTML = text;
-    this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px';
+    //this.elem.style.left = (300 - this.elem.offsetWidth / 2) + 'px';
   }
 };
+
+var base_url = "http://54.243.221.116:8000/";
 
 function httpGet(theUrl) {
   var xmlHttp = new XMLHttpRequest();
@@ -31,334 +33,10 @@ function httpGet(theUrl) {
   xmlHttp.send();
   return xmlHttp.responseText;
 }
-
-
+      
 function init(){
-    //init data
-    var json = {
-	"children": [{"children": [], "data": [], "id": "dog", "name": "dog"}, {"children": [], "data": [], "id": "cat", "name": "cat"}, {"children": [], "data": [], "id": "human", "name": "human"}, {"children": [], "data": [], "id": "animal", "name": "animal"}, {"children": [], "data": [], "id": "person", "name": "person"}, {"children": [], "data": [], "id": "horse", "name": "horse"}, {"children": [], "data": [], "id": "child", "name": "child"}, {"children": [], "data": [], "id": "snake", "name": "snake"}, {"children": [], "data": [], "id": "bird", "name": "bird"}, {"children": [], "data": [], "id": "mouse", "name": "mouse"}], "data": [], "id": "dog", "name": "dog"};
-/*
-        "id": "347_0",
-        "name": "Nine Inch Nails",
-        "children": [{
-            "id": "126510_1",
-            "name": "Jerome Dillon",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": [{
-                "id": "52163_2",
-                "name": "Howlin' Maggie",
-                "data": {
-                    "band": "Jerome Dillon",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "324134_3",
-                "name": "nearLY",
-                "data": {
-                    "band": "Jerome Dillon",
-                    "relation": "member of band"
-                },
-                "children": []
-            }]
-        }, {
-            "id": "173871_4",
-            "name": "Charlie Clouser",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": []
-        }, {
-            "id": "235952_5",
-            "name": "James Woolley",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": []
-        }, {
-            "id": "235951_6",
-            "name": "Jeff Ward",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": [{
-                "id": "2382_7",
-                "name": "Ministry",
-                "data": {
-                    "band": "Jeff Ward",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "2415_8",
-                "name": "Revolting Cocks",
-                "data": {
-                    "band": "Jeff Ward",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "3963_9",
-                "name": "Pigface",
-                "data": {
-                    "band": "Jeff Ward",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "7848_10",
-                "name": "Lard",
-                "data": {
-                    "band": "Jeff Ward",
-                    "relation": "member of band"
-                },
-                "children": []
-            }]
-        }, {
-            "id": "235950_11",
-            "name": "Richard Patrick",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": [{
-                "id": "1007_12",
-                "name": "Filter",
-                "data": {
-                    "band": "Richard Patrick",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "327924_13",
-                "name": "Army of Anyone",
-                "data": {
-                    "band": "Richard Patrick",
-                    "relation": "member of band"
-                },
-                "children": []
-            }]
-        }, {
-            "id": "2396_14",
-            "name": "Trent Reznor",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": [{
-                "id": "3963_15",
-                "name": "Pigface",
-                "data": {
-                    "band": "Trent Reznor",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "32247_16",
-                "name": "1000 Homo DJs",
-                "data": {
-                    "band": "Trent Reznor",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "83761_17",
-                "name": "Option 30",
-                "data": {
-                    "band": "Trent Reznor",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "133257_18",
-                "name": "Exotic Birds",
-                "data": {
-                    "band": "Trent Reznor",
-                    "relation": "member of band"
-                },
-                "children": []
-            }]
-        }, {
-            "id": "36352_19",
-            "name": "Chris Vrenna",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": [{
-                "id": "1013_20",
-                "name": "Stabbing Westward",
-                "data": {
-                    "band": "Chris Vrenna",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "3963_21",
-                "name": "Pigface",
-                "data": {
-                    "band": "Chris Vrenna",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "5752_22",
-                "name": "Jack Off Jill",
-                "data": {
-                    "band": "Chris Vrenna",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "33602_23",
-                "name": "Die Warzau",
-                "data": {
-                    "band": "Chris Vrenna",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "40485_24",
-                "name": "tweaker",
-                "data": {
-                    "band": "Chris Vrenna",
-                    "relation": "is person"
-                },
-                "children": []
-            }, {
-                "id": "133257_25",
-                "name": "Exotic Birds",
-                "data": {
-                    "band": "Chris Vrenna",
-                    "relation": "member of band"
-                },
-                "children": []
-            }]
-        }, {
-            "id": "236021_26",
-            "name": "Aaron North",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": []
-        }, {
-            "id": "236024_27",
-            "name": "Jeordie White",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": [{
-                "id": "909_28",
-                "name": "A Perfect Circle",
-                "data": {
-                    "band": "Jeordie White",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "237377_29",
-                "name": "Twiggy Ramirez",
-                "data": {
-                    "band": "Jeordie White",
-                    "relation": "is person"
-                },
-                "children": []
-            }]
-        }, {
-            "id": "235953_30",
-            "name": "Robin Finck",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": [{
-                "id": "1440_31",
-                "name": "Guns N' Roses",
-                "data": {
-                    "band": "Robin Finck",
-                    "relation": "member of band"
-                },
-                "children": []
-            }]
-        }, {
-            "id": "235955_32",
-            "name": "Danny Lohner",
-            "data": {
-                "band": "Nine Inch Nails",
-                "relation": "member of band"
-            },
-            "children": [{
-                "id": "909_33",
-                "name": "A Perfect Circle",
-                "data": {
-                    "band": "Danny Lohner",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "1695_34",
-                "name": "Killing Joke",
-                "data": {
-                    "band": "Danny Lohner",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "1938_35",
-                "name": "Methods of Mayhem",
-                "data": {
-                    "band": "Danny Lohner",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "5138_36",
-                "name": "Skrew",
-                "data": {
-                    "band": "Danny Lohner",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "53549_37",
-                "name": "Angkor Wat",
-                "data": {
-                    "band": "Danny Lohner",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "113510_38",
-                "name": "Puscifer",
-                "data": {
-                    "band": "Danny Lohner",
-                    "relation": "member of band"
-                },
-                "children": []
-            }, {
-                "id": "113512_39",
-                "name": "Renhold\u00ebr",
-                "data": {
-                    "band": "Danny Lohner",
-                    "relation": "is person"
-                },
-                "children": []
-            }]
-        }],
-        "data": []
-    };
-*/
-    //end
     var infovis = document.getElementById('infovis');
-    var w = infovis.offsetWidth - 50, h = infovis.offsetHeight - 50;
+    var w = infovis.offsetWidth - 50, h = infovis.offsetHeight - 150;
     
     //init Hypertree
     var ht = new $jit.Hypertree({
@@ -370,31 +48,36 @@ function init(){
       //Change node and edge styles such as
       //color, width and dimensions.
       Node: {
+          overridable: true,
           dim: 9,
           color: "#f00"
       },
+      //transition: $jit.Trans.Back.easeOut,
+      duration:1000,
       Edge: {
+          overridable: true,
           lineWidth: 2,
           color: "#088"
       },
       onBeforeCompute: function(node){
           Log.write("centering");
       },
+      onBeforePlotLine: function(adj) {
+          //adj.data.$lineWidth = Math.random() * 17 + 1;
+          console.log(adj.data);
+      },
       //Attach event handlers and add text to the
       //labels. This method is only triggered on label
       //creation
       onCreateLabel: function(domElement, node){
-          domElement.innerHTML = node.name;
-          $jit.util.addEvent(domElement, 'click', function () {
-		  var new_name = JSON.parse(httpGet("http://54.243.221.116:8000/d4d/".concat(node.id.replace(' ', '_'))));
-		  ht.loadJSON(new_name);
-		  ht.refresh();
-                  ht.onClick(new_name.id, {
-                  onComplete: function() {
-                      ht.controller.onComplete();
-                  }
-              });
+        domElement.innerHTML = node.name;
+        $jit.util.addEvent(domElement, 'click', function () {
+          ht.onClick(node.id, {
+            onComplete: function() {
+                ht.controller.onComplete();
+            }
           });
+        });
       },
       //Change node styles when labels are placed
       //or moved.
@@ -426,6 +109,17 @@ function init(){
           //This is done by collecting the information (stored in the data property) 
           //for all the nodes adjacent to the centered node.
           var node = ht.graph.getClosestNodeToOrigin("current");
+          // Parse assertion simply, assume it is of the form "concept relation concept"
+          var assertion = node.id
+          var relation = $("#assertion").val();
+          var relation_index = assertion.indexOf(relation);
+          var left = assertion.substring(0, relation_index-1).replace(" ", "_");
+          var right = assertion.substring(relation_index+relation.length+1).replace(" ", "_");
+          var query = "similar/".concat(left).concat("/").concat(relation).concat("/").concat(right).concat("/10/");
+          var new_name = JSON.parse(httpGet(base_url.concat(query)));
+          console.log(new_name);
+          ht.loadJSON(new_name);
+          ht.refresh();
           var html = "<h4>" + node.name + "</h4><b>Connections:</b>";
           html += "<ul>";
           node.eachAdjacency(function(adj){
@@ -439,10 +133,17 @@ function init(){
           $jit.id('inner-details').innerHTML = html;
       }
     });
-    //load JSON data.
-    ht.loadJSON(json);
-    //compute positions and plot.
-    ht.refresh();
-    //end
-    ht.controller.onComplete();
+    $("#assertionSelect").submit(function() {
+      var left = $("#leftValue").val().replace(" ", "_");
+      var right = $("#rightValue").val().replace(" ", "_");
+      var query = "similar/".concat(left).concat("/").concat($("#assertion").val()).concat("/").concat(right).concat("/10/");
+      var new_name = JSON.parse(httpGet(base_url.concat(query)));
+      ht.loadJSON(new_name);
+      ht.refresh();
+      ht.onClick(new_name.id, {
+        onComplete: function() {
+            ht.controller.onComplete();
+        }
+      });
+    });
 }
--- d4d/views.py
+++ d4d/views.py
@@ -8,20 +8,55 @@ from d4d import d4d
 # Create your views here.
 
 explorer = d4dExplorer()
+mini = 0
+maxi = 0
+
+def normalize_truth_score(score):
+  global maxi, mini
+  if maxi == 0:
+    maxi = 1
+  return (score - mini) / (maxi - mini)
 
 def get_similar_assertions(left, relation, right):
   similar_left = d4d.c4.similar_concepts_to(left)
   similar_right = d4d.c4.similar_concepts_to(right)
-  similar_assertions = ["%s %s %s" % (str(left[0]), relation, str(right[0]))
+  similar_assertions = [("%s %s %s" % (str(left[0]), relation, str(right[0])),
+                         left[1] * right[1])
                         for left in similar_left
                         for right in similar_right]
-  similar_with_score = [(str(assertion), d4d.c4.how_true_is(str(assertion)))
+  # Rank and sort based on how "true" they are
+  similar_with_score = [(str(assertion[0]), d4d.c4.how_true_is(str(assertion[0])), assertion[1])
                         for assertion in similar_assertions]
   similar_with_score.sort(key=lambda x: x[1], reverse=True)
-  return similar_with_score
+  # Normalize before we return so we can use a 0-1 slider
+  global maxi, mini
+  maxi = max([score[1] for score in similar_with_score])
+  mini = min([score[1] for score in similar_with_score])
+  normalized = dict([(normalize_truth_score(assertion[1]), (assertion[0], assertion[2]))
+                for assertion in similar_with_score])
+  return normalized
 
 def similar_endpoint(request, left, relation, right, count):
-  return HttpResponse(str(get_similar_assertions(left, relation, right)[:int(count)]))
+  left = left.replace("_", " ")
+  right = right.replace("_", " ")
+  assertion = "%s %s %s" % (left, relation, right)
+  similar_assertions = get_similar_assertions(left, relation, right)[:int(count)]
+  return HttpResponse(json.dumps(out))
+  out = [{"id": assertion,
+         "name": assertion,
+         "data": {"$dim": 30 * normalize_truth_score(d4d.c4.how_true_is(assertion))},
+         "adjacencies": [
+          {"nodeTo": a[0],
+           "data":
+            {"$lineWidth": 16}
+          } for a in similar_assertions]}]
+  out = out + [{"id": a[0],
+                "name": a[0],
+                "data": {"$dim": 30*a[1]},
+                "adjacencies": {"nodeTo": assertion,
+                                "data": {"$lineWidth": 16}}
+               } for a in similar_assertions]
+  return HttpResponse(json.dumps(out))
 
 def get_children(concept, exclude):
   explorer.concept = concept
